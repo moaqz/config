bced77bd116ab76b2e885affa65b72d1b30c1a01
moaqz <moaqz.dev@gmail.com>
2025-04-08 14:17:07 -0300

    update eslint version and remove unnecessary rules

--- eslint.config.js
+++ eslint.config.js
@@ -2,10 +2,4 @@ import moaqz from "eslint-config-moaqz";
 
 export default [
   ...moaqz,
-  {
-    rules: {
-      "@stylistic/space-before-function-paren": "off",
-      "no-console": "off"
-    }
-  }
 ];
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
+/* eslint-disable no-console */
 
 import process from "node:process";
 import { parseArgs, styleText } from "node:util";
